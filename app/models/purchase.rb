ea4fc1c51995425bd14cab8de9648b13b727a167
Aaron Grant <aarongeoffreygrant5@hotmail.com>
2015-08-30 17:41:57 -0700

    Fixed the problems with purchase.rb and purchase_mailer.html.erb.

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,12 +1,14 @@
 class Purchase < ActiveRecord::Base
 	after_create :email_purchaser
 
-private
-
-def email_purchaser
-   PurchaseMailer.purchase_receipt(self).deliver
-end
 	def to_param
 		uuid
 	end
+
+	private
+
+	def email_purchaser
+   		PurchaseMailer.purchase_receipt(self).deliver
+	end
+	
 end
