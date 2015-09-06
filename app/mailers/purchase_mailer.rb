65c4bc13293f26b44fc0a2e2124cffdaa96be6d5
Aaron Grant <aarongeoffreygrant5@hotmail.com>
2015-09-06 12:43:10 -0700

    Made the purchase_mailer.rb and the purchase_mailer.html.erb just like the instructor did and am trying to fix the error message

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,7 +1,7 @@
 class PurchaseMailer < ActionMailer::Base
 layout 'purchase_mailer'
 
- default from: "Interactive Artwork <aarongeoffreygrant5@hotmail.com>"
+ default from: "One Month Stripe Payments <payments@onemonth.com>"
 
  def purchase_receipt purchase
  @purchase = purchase
